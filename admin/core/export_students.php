99e0000c73cd12116602812266b6a7e1e9b6865c
Thabiso Nthako <thabiso.nthako@gmail.com>
2025-05-23 09:14:13 +0200

    added export students to admin

---
+++ admin/core/export_students.php
@@ -0,0 +1,63 @@
+<?php
+chdir("../../");
+session_start();
+require_once "db/config.php";
+
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    $class = $_POST["class"];
+
+    try {
+        $conn = new PDO(
+            "mysql:host=" .
+                DBHost .
+                ";dbname=" .
+                DBName .
+                ";charset=" .
+                DBCharset .
+                ";collation=" .
+                DBCollation .
+                ";prefix=" .
+                DBPrefix .
+                "",
+            DBUser,
+            DBPass
+        );
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        $stmt = $conn->prepare("SELECT * FROM tbl_classes WHERE id = ?");
+        $stmt->execute([$class]);
+        $result = $stmt->fetchAll();
+
+        $fileName = $result[0][1] . " student numbers" . ".csv";
+        $_SESSION["export_file"] = $fileName;
+
+        if (file_exists("import_sheets/" . $fileName)) {
+            unlink("import_sheets/" . $fileName);
+        }
+
+        $fp = fopen("import_sheets/" . $fileName, "w");
+
+        $rowData = ["STUDENT NUMBER", "STUDENT NAME"];//];
+        fputcsv($fp, $rowData);
+
+        $stmt = $conn->prepare("SELECT * FROM tbl_students WHERE class = ?");
+        $stmt->execute([$class]);
+        $result = $stmt->fetchAll();
+
+        foreach ($result as $row) {
+            $rowData = [
+                $row[0],
+                "" . $row[1] . " " . $row[2] . " " . $row[3] . "",
+                // "0",
+            ];
+            fputcsv($fp, $rowData);
+        }
+
+        header("location:../export_students");
+    } catch (PDOException $e) {
+        echo "Connection failed: " . $e->getMessage();
+    }
+} else {
+    header("location:../");
+}
+?>
